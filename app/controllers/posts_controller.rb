566b91004597d4f18fdd2acf35da1f955a91f517
yanagi_prg <takayanagi.prg@gmail.com>
2019-07-03 05:05:48 +0900

    finish CRUD Posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,39 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to posts_path
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    redirect_to post_path(@post) if @post.update(post_params)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+private
+  def post_params
+    params.require(:post).permit(:name)
+  end
+
+end
